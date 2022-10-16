5be35946fe46a0596bcb050a64d1568e0e714d9a
Ismail8396 <iska8396@gmail.com>
2022-10-16 19:46:46 +0400

    Added new themes.
    Known bug: Selecting a theme cause an error.

--- src/main/java/gui/panels/dialogs/AppearanceDialog.java
+++ src/main/java/gui/panels/dialogs/AppearanceDialog.java
@@ -57,6 +57,7 @@ public class AppearanceDialog extends JPanel {
                         "Vuesion",
 
                 };
+
         JScrollPane scrollPane = new JScrollPane();
         themeList = new JList(ThemeNames);
         themeList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
